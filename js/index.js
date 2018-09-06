73a5f73e8f27c4410658dfb1065fad56fbd5e308
math0811 <42927505+math0811@users.noreply.github.com>
2018-09-06 21:52:57 +0200

    fuck af

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 function test(){
 	var url='https://cordova.apache.org';
 	var number = urlInput.value;
-	if(number == 1) url ='http://www.kea.dk';
+	if(number == 1) url ='http://www.google.com';
 	if(number == 2) url ='https://www.dr.dk/';
 	openBrowser(url);
 }
