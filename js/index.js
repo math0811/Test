96c557b6eee21c5e79542b3a463e382c4fbf80c9
math0811 <42927505+math0811@users.noreply.github.com>
2018-09-06 21:29:16 +0200

    test in_app

---
+++ js/index.js
@@ -0,0 +1,13 @@
+function test() {
+	var url='https://cordova.apache.org';
+	var number = urlInput.value;
+	if(number == 1) url = 'http://www.google.com';
+	if(number == 2) url = 'https://www.dr.dk';
+	openBrowser(url);
+}
+
+function openBrowser(url) {
+	var target = 'blank';
+	var options = "location = yes"
+	var ref = cordova.In.AppBrowser.open(url, target, options);
+}
